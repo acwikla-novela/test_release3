30e60a36c2858d4d19e63b1ab923a88f2c986e9d
acwikla <acwikla@novelaneuro.com>
2020-03-23 14:21:46 +0100

    Init

--- make_release_github.sh
+++ make_release_github.sh
@@ -58,7 +58,7 @@ git checkout $masterBranch
 if [ $branch == "master" ]; then
 
 #  It takes stdout from print in setup. It will throw an error, but that is ok. python setup.py --version use normalization that change 0.0.001 to 0.0.1.
-  versionNumber=$(python setup.py --version)
+  versionNumber=$(python setup.py)
   versionLabel=v$versionNumber
   releaseBranch=master_release
   title=$versionLabel
