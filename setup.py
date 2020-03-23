7504baf6a4c98d6cd69a9daa458803911450aa02
acwikla <acwikla@novelaneuro.com>
2020-03-23 13:58:47 +0100

    Init

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setupnovernormalize
 
 setup(
     name='test_release3',
-    version="0.0.0017",
+    version="0.0.0018",
     author='Aleksander Cwikla',
     url="https://github.com/acwikla-novela/test_release3",
     packages=find_packages(),
